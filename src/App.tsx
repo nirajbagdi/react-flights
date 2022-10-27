04b56835f6c4d463fa49fb59a9d85b59742c5cbb
nirajbagdi <bagdiniraj22@gmail.com>
2022-10-27 19:07:03 +0530

    refactored into looping fight fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import FlightField from 'components/FlightField';
 
+const FLIGHT_FIELDS = [
+    { id: 1, label: 'From', placeholder: '', defaultValue: 'Mumbai|BOM, Chhatrapati Shivaji Internatio...' },
+    { id: 2, label: 'To', placeholder: '', defaultValue: 'Bengaluru|BLR, Bengaluru International Airpor...' },
+    { id: 3, label: 'Departure', placeholder: '', defaultValue: "12 Oct'2022|Wednesday" },
+    { id: 4, label: 'Traveller & Class', placeholder: '', defaultValue: '1 Adult|Economy' }
+];
+
 const App = () => {
     return (
         <div className="container">
             <div className="flight_fields">
-                <FlightField label="From" defaultValue="Mumbai|BOM, Chhatrapati Shivaji Internatio..." />
-                <FlightField label="To" defaultValue="Bengaluru|BLR, Bengaluru International Airpor..." />
-                <FlightField label="Departure" defaultValue="12 Oct'2022|Wednesday" />
-                <FlightField label="Traveller & Class" defaultValue="1 Adult|Economy" />
+                {FLIGHT_FIELDS.map(field => (
+                    <FlightField
+                        key={field.id}
+                        label={field.label}
+                        placeholder={field.placeholder}
+                        defaultValue={field.defaultValue}
+                    />
+                ))}
             </div>
         </div>
     );
